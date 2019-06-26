5b729e2d2da1eef154b093a2c9ad9141e8fdeb17
hideo54 <hideo54@hideo54.com>
2019-06-27 05:12:19 +0900

    Create a module to send an audio buffer to Google Home

---
+++ lib/GoogleHome.ts
@@ -0,0 +1,40 @@
+import * as http from 'http';
+import { AddressInfo } from 'net';
+import { Client as GoogleCastClient, DefaultMediaReceiver } from 'castv2-client';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export class GoogleHome {
+    IPAddress: string;
+
+    constructor(ip: string) {
+        this.IPAddress = ip;
+    }
+
+    pushAudio(audio) {
+        const client = new GoogleCastClient();
+        client.connect( this.IPAddress, () => {
+            client.launch(DefaultMediaReceiver, (err, player) => {
+                if (err) {
+                    console.error(err);
+                    client.close();
+                }
+                const server = http.createServer((request, response) => {
+                    response.setHeader('Content-Type', 'audio/mpeg');
+                    response.end(audio);
+                });
+                server.listen(0);
+                const address = server.address() as AddressInfo;
+                const media = {
+                    contentId: `http://${process.env.SERVER_ADDRESS}:${address.port}`,
+                    contentType: 'audio/mp3',
+                    streamType: 'BUFFERED'
+                };
+                player.load(media, { autoplay: true }, (err, status) => {
+                    server.close();
+                    client.close();
+                });
+            });
+        });
+    }
+}
