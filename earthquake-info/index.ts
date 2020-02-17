161629fa5419103c37bb9ca86f5480f1b824bac4
hideo54 <hideo54@hideo54.com>
2020-02-17 17:17:49 +0900

    Minor fix for running env

--- earthquake-info/index.ts
+++ earthquake-info/index.ts
@@ -1,10 +1,11 @@
 import { GoogleHome } from '../lib/GoogleHome';
 import * as Speech from '../lib/Speech';
-import * as EventSource from 'eventsource';
-import * as htmlToText from 'html-to-text';
+import EventSource from 'eventsource';
+import htmlToText from 'html-to-text';
 
 const streamUrl = `https://unnerv.jp/api/v1/streaming/hashtag?tag=${encodeURI('地震')}`;
 const es = new EventSource(streamUrl);
+// @ts-ignore
 es.addEventListener('update', async event => {
     const data = JSON.parse(event.data);
     const content = htmlToText.fromString(data.content, {
