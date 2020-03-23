ccf9f2d08868ba2dd684fc108b0c7955eeed6061
hideo54 <hideo54@hideo54.com>
2020-03-24 02:35:00 +0900

    Create auto-dimmer program for Hue, replacing custom lib to public one

---
+++ HueLamps/index.ts
@@ -0,0 +1,39 @@
+import { v3 as hueAPIv3 } from 'node-hue-api';
+import Api from 'node-hue-api/lib/api/Api';
+const hueAPI = hueAPIv3.api;
+const LightState = hueAPIv3.lightStates.LightState;
+
+interface SimpleLightState {
+    on: boolean;
+    bri: number;
+    ct: number;
+    alert: 'select';
+    colormode: 'ct' | 'xy';
+    mode: 'homeautomation';
+    reachable: boolean;
+}
+
+interface ColorfulLightState extends SimpleLightState {
+    hue: number;
+    sat: number;
+    effect: 'none';
+    xy: [ number, number ];
+}
+
+const colorfulLightIDs = [ 1 ];
+const simpleLightIDs = [ 2, 3 ];
+const allLightIDs = [...colorfulLightIDs, ...simpleLightIDs];
+
+// This program assumes that all lamps are regarded as an integrated light system and are controlled by hideout system.
+// So every lamps has a same brightness.
+
+export const dimLightsBy5 = async (bridge: Api) => {
+    const currentState: SimpleLightState = await bridge.lights.getLightState(allLightIDs[0]);
+    const newBri = Math.max(currentState.bri - 5, 0);
+    const dimmerState = newBri === 0
+        ? new LightState().off()
+        : new LightState().on(true).bri(newBri);
+    for (const lightID of allLightIDs) {
+        await bridge.lights.setLightState(lightID, dimmerState);
+    }
+};
--- index.ts
+++ index.ts
@@ -11,3 +11,15 @@ require('./earthquake-info');
 schedule.scheduleJob('   */ 5 * * * *', clockJob);
 schedule.scheduleJob('10 */10 * * * *', comfortabilityJob);
 schedule.scheduleJob('30   10 * * * *', nhkRadioNewsJob);
+
+import { v3 as hueAPIv3 } from 'node-hue-api';
+import { dimLightsBy5 } from './HueLamps';
+const hueAPI = hueAPIv3.api;
+(async () => {
+    const address = process.env.HUE_BRIDGE_ADDRESS!;
+    const username = process.env.HUE_BRIDGE_USERNAME!;
+    const bridge = await hueAPI.createLocal(address).connect(username);
+    schedule.scheduleJob('* 0-6 * * *', async () => {
+        await dimLightsBy5(bridge);
+    });
+})();
