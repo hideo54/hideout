123860ff3f23fbf2aadda963a01f3acc787e72eb
hideo54 <hideo54@hideo54.com>
2020-04-14 22:13:31 +0900

    二酸化炭素濃度読み上げを改善

--- CO2/index.ts
+++ CO2/index.ts
@@ -5,11 +5,11 @@ import { GoogleHome } from '../lib/GoogleHome';
 export default async (sensor: MHZ19) => {
     const value = await sensor.readCO2();
     if (value < 500) return;
-    let phrase = `現在の二酸化炭素濃度は${value}ppmです。`;
+    let phrase = `現在の二酸化炭素濃度は ${value} ppmです。`;
     if (value >= 1000) phrase += '今すぐ換気してください。';
 
     const speaker = new Speech.Speech();
     const gHome = new GoogleHome(process.env.GOOGLE_HOME_ADDRESS);
-    const voice = await speaker.getJPVoice(phrase, Speech.JPSpeaker.Gentleman);
+    const voice = await speaker.getJPVoice(phrase, Speech.JPSpeaker.Woman);
     gHome.pushAudio(voice);
 };
