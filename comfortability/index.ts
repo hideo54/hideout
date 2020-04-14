0789daf4d44cad1ee002a5a33db142caedc2db41
hideo54 <hideo54@hideo54.com>
2020-04-14 22:59:21 +0900

    Minor fix

--- comfortability/index.ts
+++ comfortability/index.ts
@@ -50,9 +50,8 @@ const generatePhrase = async () => {
     }
     if (advice) {
         return report + advice;
-    } else {
-        return null;
     }
+    return;
 };
 
 export default async (date: Date) => {
