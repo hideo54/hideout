fcbb552e755ca9294899aaf7991f07e153b5dff3
hideo54 <hideo54@hideo54.com>
2020-04-26 16:28:05 +0900

    Add Slack web client to the utils for future development

--- index.ts
+++ index.ts
@@ -2,30 +2,30 @@ import schedule from 'node-schedule';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import BME280 from 'bme280-sensor';
-import MHZ19 from 'mh-z19b';
-
-import { v3 as hueAPIv3 } from 'node-hue-api';
-const hueAPI = hueAPIv3.api;
-
 import { Speech } from './lib/speech';
 import { GoogleHome } from './lib/google-home';
+import { WebClient as SlackWebClient } from '@slack/web-api';
+import BME280 from 'bme280-sensor';
+import MHZ19 from 'mh-z19b';
+import { v3 as hueAPIv3 } from 'node-hue-api';
+const hueAPI = hueAPIv3.api;
 
 const init = async () => {
     const speaker = new Speech();
     const gHome = new GoogleHome(process.env.GOOGLE_HOME_ADDRESS!);
+    const slackWebClient = new SlackWebClient(process.env.SLACK_USER_TOKEN!);
     const bme280 = new BME280({
         i2cBusNo: 1,
         i2cAddress: 0x76,
         outputsInfo: false,
-});
+    });
     await bme280.init();
     const mhz19 = new MHZ19();
     const hueAddress = process.env.HUE_BRIDGE_ADDRESS!;
     const hueUsername = process.env.HUE_BRIDGE_USERNAME!;
     const hueBridge = await hueAPI.createLocal(hueAddress).connect(hueUsername);
     const utils = {
-        speaker, gHome, bme280, mhz19, hueBridge,
+        speaker, gHome, slackWebClient, bme280, mhz19, hueBridge,
     } as Utils;
     return utils;
 };
@@ -57,4 +57,4 @@ init().then(utils => {
     schedule.scheduleJob('* 9-12 * * *', async () => {
         await brightenLightsBy5(utils.hueBridge);
     });
-    });
+});
--- types/global.d.ts
+++ types/global.d.ts
@@ -3,11 +3,13 @@ import { GoogleHome } from '../lib/google-home';
 import BME280 from 'bme280-sensor';
 import MHZ19 from 'mh-z19b';
 import HueAPI from 'node-hue-api/lib/api/Api';
+import { WebClient as SlackWebClient } from '@slack/web-api';
 
 declare global {
     interface Utils {
         speaker: Speech,
         gHome: GoogleHome,
+        slackWebClient: SlackWebClient,
         bme280: BME280,
         mhz19: MHZ19,
         hueBridge: HueAPI,
