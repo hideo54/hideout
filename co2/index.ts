6dc07d2714a4def4710948dd87d06c974a361c15
hideo54 <hideo54@hideo54.com>
2020-05-13 23:47:36 +0900

    Minor improvement

--- co2/index.ts
+++ co2/index.ts
@@ -1,9 +1,7 @@
-import MHZ19 from 'mh-z19b';
-
 const job: Job = async (date: Date, utils: Utils) => {
     const value = await utils.mhz19.readCO2();
     if (value < 1000) return;
-    let phrase = `現在の二酸化炭素濃度は ${value} ppmです。今すぐ換気してください。`;
+    let phrase = `現在の二酸化炭素濃度は ${value} ピーピーエムです。今すぐ換気してください。`;
     const voice = await utils.speaker.getJPVoice(phrase, 'boy');
     utils.gHome.pushAudio(voice);
 };
