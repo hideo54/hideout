f663d03c316ef3490f1cb7dfba74231f844970a1
hideo54 <hideo54@hideo54.com>
2020-07-29 17:51:56 +0900

    Fix test

--- clock/index.test.ts
+++ clock/index.test.ts
@@ -1,17 +1,17 @@
 import { generatePhrase } from './index';
 
 describe('clock', () => {
-    it('Returns correct text at *:00', async () => {
+    it('Returns correct text at *:00', () => {
         const date = new Date('2020-01-01T10:00:00');
         const phrase = generatePhrase(date);
         expect(phrase).toBe('時刻は10時です。');
     });
-    it('Returns correct text at *:30', async () => {
+    it('Returns correct text at *:30', () => {
         const date = new Date('2020-01-01T10:30:00');
         const phrase = generatePhrase(date);
         expect(phrase).toBe('時刻は10時半です。');
     });
-    it('Returns correct text at *:54', async () => {
+    it('Returns correct text at *:54', () => {
         const date = new Date('2020-01-01T10:54:00');
         const phrase = generatePhrase(date);
         expect(phrase).toBe('時刻は10時54分です。');
