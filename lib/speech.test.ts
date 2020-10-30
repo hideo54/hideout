63cfa233025ba97e2bd9187b086d98a59f3ffb71
hideo54 <hideo54@hideo54.com>
2020-10-30 17:13:19 +0900

    Fix test

--- lib/speech.test.ts
+++ lib/speech.test.ts
@@ -3,12 +3,7 @@ import Speech from './speech';
 describe('lib/speech', () => {
     it('returns voice buffer correctly', async () => {
         const speech = new Speech();
-        const voice = await speech.getJPVoice('test', 'gentleman');
+        const voice = await speech.getJPVoice('test', { speaker: 'gentleman'});
         expect(voice).toStrictEqual(Buffer.from('test by ja-JP-Wavenet-D'));
-    });
-    it('returns woman voice buffer if not specified', async () => {
-        const speech = new Speech();
-        const voice = await speech.getJPVoice('test');
-        expect(voice).toStrictEqual(Buffer.from('test by ja-JP-Wavenet-B'));
     });
 });
