5c192f44e83fcc4d75d28f2ff4ac199369a7edf4
Hideo Yasumoto <hideo54@hideo54.com>
2021-11-15 16:41:34 +0900

    Use sensor only in production

--- index.ts
+++ index.ts
@@ -7,10 +7,13 @@ import MHZ19 from 'mh-z19b';
 import * as dns from './dns';
 
 class Sensors {
+    disabled: boolean = true;
     bme280: BME280 | null = null;
     mhz19: MHZ19 | null = null;
 
-    constructor() {
+    constructor({ disabled }: { disabled: boolean; } = { disabled: false }) {
+        this.disabled = disabled;
+        if (this.disabled) return;
         this.bme280 = new BME280({
             i2cBusNo: 1,
             i2cAddress: 0x76,
@@ -20,13 +23,16 @@ class Sensors {
     }
 
     async init() {
+        if (this.disabled) return;
         await this.bme280?.init();
     }
 }
 
 const init = async () => {
-    const sensors = new Sensors();
-    await sensors.init();
+    if (process.env.NODE_ENV === 'production') {
+        const sensors = new Sensors();
+        await sensors.init();
+    }
 
     dns.init();
 };
